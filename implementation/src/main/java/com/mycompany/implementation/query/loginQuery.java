0797f483b4ec9ed8054fe39d4be3d354670bdf07
Raymun <Raymun1@hotmail.co.uk>
2018-03-20 01:23:42 +0000

    fixed login screen

--- implementation/src/main/java/com/mycompany/implementation/query/loginQuery.java
+++ implementation/src/main/java/com/mycompany/implementation/query/loginQuery.java
@@ -11,6 +11,8 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -23,7 +25,7 @@ public class loginQuery {
     private ResultSet r;
     
     public loginQuery(String dbname, String user, String pass){
-        String url = "jdbc:mysql://localhost:3306/new_schema?zeroDateTimeBehavior=convertToNull"; //db location
+        String url = "jdbc:mysql://localhost:3306/sys?zeroDateTimeBehavior=convertToNull"; //db location
         try {
             Class.forName("com.mysql.jdbc.Driver"); //make instance of driver
             this.c = DriverManager.getConnection(url,user,pass); //connect to db
@@ -42,11 +44,16 @@ public class loginQuery {
        
         
         try {
-            String query = "SELECT * FROM staff WHERE staff.username = " + user + " AND staff.password = "+ pass + ";" ;
-            PreparedStatement s = this.c.prepareStatement(query); //create statement 
-            this.r = s.executeQuery(); //execute statement
-            r.next();
-            return r.getObject("userType").toString(); //returns if any rows are found   
+            String[] theuser = user.split("\\."); 
+            if (theuser.length >1){
+                String query = "SELECT * FROM staff WHERE staff.firstName = '" + theuser[0] + "' AND staff.surName = '" +theuser[1] +"' AND staff.password = "+ pass + ";" ;
+                System.out.println(query);
+                PreparedStatement s = this.c.prepareStatement(query); //create statement 
+                this.r = s.executeQuery(); //execute statement
+                r.next();
+                return r.getObject("employeeType").toString(); //returns if any rows are found
+            }
+            return null;
         } catch (SQLException ex) {
             Logger.getLogger(loginQuery.class.getName()).log(Level.SEVERE, null, ex);
             return null;
